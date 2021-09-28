b10f71153caf87e99ff4c13e46724b2279f5a343
Myles <mylesmatthews2112@gmail.com>
2021-09-28 14:19:59 +0000

    1-5 Completed

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 //1
 function countToFifty(){
-  
+  for (let i = 1; i <= 50; i+=1){
+      console.log(i)
+  }
 }
 
 //2
-function countFromOne(){
+function countFromOne(number){
+    for (let i = 1; i <= number; i+=1){
+        console.log(i)
+    }
   
 }
 
 //3
 function threeAndFiveInFifty(){
+    for (let i = 1; i <= 50; i+=1){
+        if (i % 3 == 0 || i % 5 == 0){
+            console.log(i)
+        }
+    }
   
 }
 
 //4
-function threeAndFive(){
+function threeAndFive(number){
+    for (let i = 1; i <= number; i+=1){
+        if (i % 3 == 0 || i % 5 == 0){
+            console.log(i)
+        }
+    }
+  
   
 }
-
-
+function triple(numbers){
+  const NewArray = numbers.map(x => x * 3);
+  return NewArray
+}
+triple([5, 20, 30])
